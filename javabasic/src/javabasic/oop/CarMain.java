2d70e78571d03ad7bc9864ba4caa0a5432af79e3
KiYongCho <152364448+KiYongCho@users.noreply.github.com>
2024-12-27 21:51:40 +0900

    Update CarMain.java

--- javabasic/src/javabasic/oop/CarMain.java
+++ javabasic/src/javabasic/oop/CarMain.java
@@ -1,72 +1,76 @@
 package javabasic.oop;
 
 public class CarMain {
-	
+
 	public static void main(String[] args) {
 		
-		// Car타입 객체를 생성
-		// 1. Car() 생성자를 호출
-		// 2. new연산을 통해서 Car타입의 객체가 메모리에 생성
-		// 3. 메모리에 생성된 객체의 주소를 해시알고리즘을 사용해서 참조로 변환
-		// 4. 참조(참조값)을 Car타입 객체참조변수인 bmw에 저장
+		// Car 객체 생성
+		// Car() 생성자 호출 > 메모리에 Car타입객체 생성 > 생성된 객체의 주소
+		// > 해시알고리즘을 사용해서 참조값(해시값)으로 변환 > 참조값을 bmw 참조변수에 저장
 		Car bmw = new Car();
+		// javabasic.oop.Car@5305068a : 클래스풀네임@객체참조값(16진수)
 		System.out.println(bmw);
+		System.out.println(bmw.hashCode()); // 객체참조값(10진수)
 		
-		// 객체의 멤버변수들에 값을 할당 (타입에 맞춰서)
+		// 객체의 멤버변수에 값을 할당
+		bmw.carCount = 1;
 		bmw.company = "BMW KOREA";
 		bmw.model = "520D";
 		bmw.name = "BMW";
 		bmw.price = 1000;
 		
-		System.out.println(
-				"제조사명 : " + bmw.company + "\n"
-				+ "모델명 : " + bmw.model + "\n"
-				+ "이름 : " + bmw.name + "\n"
-				+ "판매가격 : " + bmw.price
-		);
+		Car bentz = new Car();
+		bentz.carCount = 2;
+		bentz.company = "BENTZ KOREA";
+		bentz.model = "AMG";
+		bentz.name = "BENTZ";
+		bentz.price = 2000;
 		
+		// bmw의 carCount 출력
+		// static 멤버변수는 메모리에 하나만 생기고
+		// 모든 객체들이 값을 공유
+		System.out.println(bmw.carCount); // 2
 		
-		// 같은 객체 = 주소가 같다 = 참조가 같다 = 참조값이 같다
-		// 메모리상에 같은 곳에 있는 객체를 같은 객체라 함
-		Car audi = new Car();
-		System.out.println(audi);
+		// bmw, bentz의 price 출력
+		// non-static 멤버변수는 객체마다 갖게되고
+		// 객체마다 다른 값을 저장
+		bmw.price = 1500;
+		System.out.println(bmw.price);
+		bentz.price = 2500;
+		System.out.println(bentz.price);
 		
-		audi.company = "AUDI KOREA";
-		audi.model = "A8";
-		audi.name = "AUDI";
-		audi.price = 1500;
+		// carCount를 클래스로 출력
+		// static 멤버변수는 클래스로 접근 가능
+		System.out.println(Car.carCount);
+		Car.carCount = 3;
+		System.out.println(bmw.carCount);
+		System.out.println(bentz.carCount);
 		
-		System.out.println(
-				"제조사명 : " + audi.company + "\n"
-				+ "모델명 : " + audi.model + "\n"
-				+ "이름 : " + audi.name + "\n"
-				+ "판매가격 : " + audi.price
-		);		
-		
-		// 메모리상에 있는 audi객체의 참조값을 복사
-		// audi2객체는 audi객체와 같은 객체 
-		Car audi2 = audi;
-		System.out.println(audi2);
-		
-		audi2.price = 2000;
-		
-		System.out.println(
-				"제조사명 : " + audi2.company + "\n"
-				+ "모델명 : " + audi2.model + "\n"
-				+ "이름 : " + audi2.name + "\n"
-				+ "판매가격 : " + audi2.price
-		);		
-		
-		System.out.println(
-				"제조사명 : " + audi.company + "\n"
-				+ "모델명 : " + audi.model + "\n"
-				+ "이름 : " + audi.name + "\n"
-				+ "판매가격 : " + audi.price
-		);			
+		// static 멤버메소드는 클래스, 객체로 접근 가능
+		System.out.println(Car.getCarCount());
+		System.out.println(bmw.getCarCount());
+		System.out.println(bentz.getCarCount());
+
+		// non-static 멤버메소드는 객체로 접근 가능
+		// System.out.println(Car.getPrice());
+		System.out.println(bmw.getPrice());
+		System.out.println(bentz.getPrice());		
 		
 	} // main
 
 } // class
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
